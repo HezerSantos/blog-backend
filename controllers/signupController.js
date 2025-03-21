0ed74e2349f250ae969d9e7f40b2c0fa1e84de74
HezerSantos <HezerSantos24@gmail.com>
2025-03-20 21:55:01 -0500

    Added sign up technical with proper validation and sanitation with error handling

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,7 +1,38 @@
-exports.signupUser = async(req, res, next) => {
-    console.log(req.body)
+const { validationResult } = require("express-validator")
+const { validateSignup } = require("../validation/signupValidator")
 
-    res.json({
-        message: "hello"
-    })
-}
+const bcrypt = require("bcryptjs")
+const prisma = require("../config/prisma")
+exports.signupUser = [
+    validateSignup,
+    async(req, res, next) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        try {
+            const { username, password } = req.body
+        
+            const hashedPassword = await bcrypt.hash(password, 10)
+    
+            await prisma.user.create({
+                data: {
+                    username: username,
+                    password: hashedPassword
+                }
+            })
+        } catch (e) {
+            console.error(e)
+            return res.status(500).json({
+                errors: {path: "network", msg: "Network Error"}
+            })
+        }
+        // console.log(username, hashedPassword)
+        res.json({
+            message: "Successfully Signed Up User"
+        })
+    }
+]
---
+++ validation/signupValidator.js
@@ -0,0 +1,44 @@
+const { body } = require("express-validator")
+const primsa = require("../config/prisma")
+const prisma = require("../config/prisma")
+
+exports.validateSignup = [
+    body("username")
+        .isLength({min: 1}).withMessage("Must contain at least one character")
+        .custom(async(username) => {
+            let user
+            try {
+                user = await prisma.user.findUnique({
+                    where: {
+                        username: username
+                    }
+                })
+                if (user){
+                    throw new Error
+                }
+                return true
+            } catch (e) {
+                if(user){
+                    throw new Error("Username Already Exists")
+                } else {
+                    throw new Error("Internal Server Error. Please Try Again")
+                }
+                
+            }
+        })
+        .trim()
+        .escape(),
+    body("password")
+        .trim()
+        .isLength({min: 3}).withMessage("Password must be at least 12 characters")
+        .escape(),
+    body("confirmPassword")
+        .trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password){
+                throw new Error("Passwords dont match")
+            }
+            return true
+        })
+        .escape()
+]
