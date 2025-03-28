8ce1f5fed5b08bd08d07e2cd9d5ebb1dd1d9d0fa
HezerSantos <HezerSantos24@gmail.com>
2025-03-28 03:37:00 -0500

    i

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -5,19 +5,20 @@ const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173", "https
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin) {
-      callback(null, true);
+      return callback(null, true);
     } 
 
     if(allowedOrigins.indexOf(origin) !== -1){
-      callback(null, true)
+      return callback(null, true)
     }else {
-      callback(new Error("CORS not allowed for this origin"), false);
+      return callback(new Error("CORS not allowed for this origin"), false);
     }
   
   },
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie", "Set-Cookie"],
   credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 200
 };
 
 module.exports = cors(corsOptions);
