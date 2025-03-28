7c51f8de6b623b514e4887d8d7faad275e80807d
HezerSantos <HezerSantos24@gmail.com>
2025-03-28 01:48:10 -0500

    Plz

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,17 +1,22 @@
 const cors = require("cors");
 
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173", "theywroteit.vercel.app"];
 
 const corsOptions = {
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
-    } else {
+    } 
+    
+    if(allowedOrigins.indexOf(origin) !== -1){
+      callback(null, true)
+    }else {
       callback(new Error("CORS not allowed for this origin"), false);
     }
+  
   },
-  methods: "GET, POST, PUT, DELETE",
-  allowedHeaders: "Content-Type,Authorization, Cookie",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   credentials: true, // Allow cookies to be sent
 };
 
