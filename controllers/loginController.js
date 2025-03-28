b839104c01c3289aa1ddb0f0d6bbe60cd9913372
HezerSantos <HezerSantos24@gmail.com>
2025-03-28 03:41:31 -0500

    Last try

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -18,6 +18,7 @@ exports.loginUser = [
                 maxAge: 60 * 60 * 1000, // 1 hour expiration time (can adjust as needed)
                 sameSite: "None", // To mitigate CSRF attacks
                 path: "/",
+                domai: "theywroteit.vercel.app/"
             });
 
             return res.json({user})
